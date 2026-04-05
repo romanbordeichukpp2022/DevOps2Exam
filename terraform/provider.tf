bef2013acd678504fe8ed019ad34351ca9c2ecca
Roman Bordeichuk <bordei4ykr@gmail.com>
2026-04-05 19:09:39 +0300

    fixed provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -5,18 +5,18 @@ terraform {
       version = "~> 2.0"
     }
   }
+
   backend "s3" {
-    endpoint                    = "fra1.digitaloceanspaces.com"
-    region                      = "us-east-1"
-    bucket                      = "bordeichuk-terraform-state"
+    endpoint                    = "https://fra1.digitaloceanspaces.com" 
+    region                      = "us-east-1" # Залишаємо заглушку для S3 API
+    bucket                      = "bordeichuk-terraform-state" 
     key                         = "terraform.tfstate"
+    
     skip_credentials_validation = true
     skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true # Виправляє помилку 403 STS
+    
+    use_path_style              = true 
   }
-}
-
-variable "do_token" {}
-
-provider "digitalocean" {
-  token = var.do_token
 }
